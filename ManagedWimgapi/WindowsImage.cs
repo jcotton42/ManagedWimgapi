df33344fc0dbddbc97a39d1b792176e774c734f0
Joshua Cotton <jcotton42@outlook.com>
2017-05-25 17:36:09 -0500

    Write constructor and implement Dispose for WindowsImage

--- ManagedWimgapi/WindowsImage.cs
+++ ManagedWimgapi/WindowsImage.cs
@@ -1,7 +1,25 @@
+using System;
+using System.Runtime.InteropServices;
+
 namespace ManagedWimgapi {
-    public sealed class WindowsImage {
-        public WindowsImage(SafeWimHandle wimHandle) {
-            throw new System.NotImplementedException();
+    public sealed class WindowsImage : IDisposable {
+        private readonly SafeWimHandle handle;
+        private bool disposed;
+
+        internal WindowsImage(SafeWimHandle wimHandle) {
+            if(handle.IsInvalid) {
+                throw Marshal.GetExceptionForHR(Marshal.GetHRForLastWin32Error());
+            }
+
+            handle = wimHandle;
+        }
+
+        /// <summary>
+        /// Unloads the windows image.
+        /// </summary>
+        public void Dispose() {
+            handle.Dispose();
+            disposed = true;
         }
     }
 }
