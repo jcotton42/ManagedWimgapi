df3e02a90108fb5ddcf50ac34415d7f8f646e88f
Joshua Cotton <jcotton42@outlook.com>
2017-05-25 00:37:18 -0400

    Write WindowsImageFile.LoadImage

---
+++ ManagedWimgapi/WindowsImage.cs
@@ -0,0 +1,7 @@
+namespace ManagedWimgapi {
+    public sealed class WindowsImage {
+        public WindowsImage(SafeWimHandle wimHandle) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- ManagedWimgapi/WindowsImageFile.cs
+++ ManagedWimgapi/WindowsImageFile.cs
@@ -10,7 +10,7 @@ namespace ManagedWimgapi {
         private bool disposed;
 
         /// <summary>
-        /// Creates or opens a Windows image at the specified path, with the specified access, mode, options, and compression type.
+        /// Creates or opens a Windows image at the specified path, with the specified access, mode, options, compression type, and temporary path.
         /// </summary>
         /// <param name="path">The path to the image file.</param>
         /// <param name="access">The access desired on the image file.</param>
@@ -19,13 +19,28 @@ namespace ManagedWimgapi {
         /// <param name="compressionType">Specifies the compression mode to be used for a newly created image file. If the file already exists, then this parameter is ignored.</param>
         /// <exception cref="Exception">TODO</exception>
         public WindowsImageFile(string path, WimAccess access, WimMode mode, WimCreationOptions options,
-            WimCompressionType compressionType) {
+            WimCompressionType compressionType, string tempPath) {
             fileHandle = NativeMethods.WIMCreateFile(path, access, mode, options, compressionType, out _);
 
             if(fileHandle.IsInvalid) {
                 throw Marshal.GetExceptionForHR(Marshal.GetHRForLastWin32Error());
                 //TODO: find out what kind of exceptions can be thrown (bad path, no permission to open, invalid parameters)
             }
+
+            if(!NativeMethods.WIMSetTemporaryPath(fileHandle, tempPath)) {
+                throw Marshal.GetExceptionForHR(Marshal.GetHRForLastWin32Error());
+                //TODO: same as last todo
+            }
+        }
+
+        /// <summary>
+        /// Loads an image inside the WIM file.
+        /// </summary>
+        /// <param name="index">The 1-based index of the image to load.</param>
+        /// <returns>A new <see cref="WindowsImage"/> representing the image.</returns>
+        /// <exception cref="Exception">TODO</exception>
+        public WindowsImage LoadImage(uint index) {
+            return new WindowsImage(NativeMethods.WIMLoadImage(fileHandle, index));
         }
 
         /// <summary>
