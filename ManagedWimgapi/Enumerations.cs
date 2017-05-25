7cf971f401047326f9da82ed149cfb25edabba2e
Joshua Cotton <jcotton42@outlook.com>
2017-05-24 23:16:12 -0400

    Reorder enums in Enumerations.cs

--- ManagedWimgapi/Enumerations.cs
+++ ManagedWimgapi/Enumerations.cs
@@ -33,47 +33,6 @@ namespace ManagedWimgapi {
     }
 
     /// <summary>
-    /// Specifies which action to take on files that exist, and which action to take when files do not exist.
-    /// </summary>
-    public enum WimMode : uint {
-        /// <summary>
-        /// Makes a new image file. Fails if the specified file already exists.
-        /// </summary>
-        CreateNew = 1,
-        /// <summary>
-        /// Makes a new image file. Overwrites if the file already exists.
-        /// </summary>
-        CreateAlways = 2,
-        /// <summary>
-        /// Opens the image file. Fails if the file does not exist.
-        /// </summary>
-        OpenExisting = 3,
-        /// <summary>
-        /// Opens the image file if it exists. If the file does not exist and the caller requests <see cref="WimAccess.Write"/> access, the function makes the file.
-        /// </summary>
-        OpenAlways = 4
-    }
-
-    /// <summary>
-    /// Specifies the options used when creating a WIM file.
-    /// </summary>
-    [Flags]
-    public enum WimCreationOptions : uint {
-        /// <summary>
-        /// Do not use any special options.
-        /// </summary>
-        None = 0,
-        /// <summary>
-        /// Generates data integrity information for new files. Verifies and updates existing files.
-        /// </summary>
-        Verify = NativeMethods.WIM_FLAG_VERIFY,
-        /// <summary>
-        /// Opens the .wim file in a mode that enables simultaneous reading and writing.
-        /// </summary>
-        ShareWrite = NativeMethods.WIM_FLAG_SHARE_WRITE
-    }
-
-    /// <summary>
     /// Specifies the compression mode on a WIM.
     /// </summary>
     public enum WimCompressionType : uint {
@@ -96,6 +55,25 @@ namespace ManagedWimgapi {
     }
 
     /// <summary>
+    /// Specifies the options used when creating a WIM file.
+    /// </summary>
+    [Flags]
+    public enum WimCreationOptions : uint {
+        /// <summary>
+        /// Do not use any special options.
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// Generates data integrity information for new files. Verifies and updates existing files.
+        /// </summary>
+        Verify = NativeMethods.WIM_FLAG_VERIFY,
+        /// <summary>
+        /// Opens the .wim file in a mode that enables simultaneous reading and writing.
+        /// </summary>
+        ShareWrite = NativeMethods.WIM_FLAG_SHARE_WRITE
+    }
+
+    /// <summary>
     /// The result of creating a WIM file.
     /// </summary>
     public enum WimCreationResult : uint {
@@ -107,5 +85,27 @@ namespace ManagedWimgapi {
         /// The file existed and was opened for access.
         /// </summary>
         OpenedExisting = 1
+    }
+
+    /// <summary>
+    /// Specifies which action to take on files that exist, and which action to take when files do not exist.
+    /// </summary>
+    public enum WimMode : uint {
+        /// <summary>
+        /// Makes a new image file. Fails if the specified file already exists.
+        /// </summary>
+        CreateNew = 1,
+        /// <summary>
+        /// Makes a new image file. Overwrites if the file already exists.
+        /// </summary>
+        CreateAlways = 2,
+        /// <summary>
+        /// Opens the image file. Fails if the file does not exist.
+        /// </summary>
+        OpenExisting = 3,
+        /// <summary>
+        /// Opens the image file if it exists. If the file does not exist and the caller requests <see cref="WimAccess.Write"/> access, the function makes the file.
+        /// </summary>
+        OpenAlways = 4
     }
 }
