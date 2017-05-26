0699ced7be38a28b23ae4f4180269116a4cf6c7b
Joshua Cotton <jcotton42@outlook.com>
2017-05-25 21:16:26 -0500

    Add WindowsImage.Apply

--- ManagedWimgapi/WindowsImage.cs
+++ ManagedWimgapi/WindowsImage.cs
@@ -14,6 +14,27 @@ namespace ManagedWimgapi {
             handle = wimHandle;
         }
 
+        private void EnsureNotDisposed() {
+            if(disposed) {
+                throw new ObjectDisposedException(GetType().FullName, "The image has already been unloaded.");
+            }
+        }
+
+        /// <summary>
+        /// Applies the contents of a windows image to the specified path.
+        /// </summary>
+        /// <param name="path">The path to apply the image to.</param>
+        /// <param name="options">The options for the application.</param>
+        /// <exception cref="Exception">TODO</exception>
+        /// <exception cref="ObjectDisposedException">The image has already been unloaded.</exception>
+        public void Apply(string path, WimApplyOptions options) {
+            EnsureNotDisposed();
+
+            if(!NativeMethods.WIMApplyImage(handle, path, options)) {
+                throw Marshal.GetExceptionForHR(Marshal.GetHRForLastWin32Error());
+            }
+        }
+
         /// <summary>
         /// Unloads the windows image.
         /// </summary>
