916c33c9f7f3b506d52fce69b579cbf8058a8a7a
Joshua Cotton <jcotton42@outlook.com>
2017-05-30 13:36:03 -0500

    Formatting

--- ManagedWimgapi/Enumerations.cs
+++ ManagedWimgapi/Enumerations.cs
@@ -10,22 +10,26 @@ namespace ManagedWimgapi {
         /// Specifies query-only access to the image file, allowing reading of image metadata, but not image contents.
         /// </summary>
         QueryOnly = 0,
+
         /// <summary>
         /// Specifies read-only access to the image file. Enables images to be applied from the file.
         /// Combine with <see cref="Write"/> for read/write (append) access.
         /// </summary>
         Read = 0x80000000,
+
         /// <summary>
         /// Specifies write access to the image file. Enables images to be captured to the file.
         /// Includes <see cref="Read"/> access to enable apply and append operations with existing images.
         /// </summary>
         Write = 0x40000000,
+
         /// <summary>
         /// Specifies mount access to the image file.
         /// Enables images to be mounted with WIMMountImageHandle.
         /// </summary>
         // TODO: replace the reference to WIMMountImageHandle
         Mount = 0x20000000,
+
         /// <summary>
         /// Convenience enumeration for read, write, and mount access.
         /// </summary>
@@ -41,33 +45,40 @@ namespace ManagedWimgapi {
         /// Specifies no apply options.
         /// </summary>
         None = 0,
+
         /// <summary>
         /// Verifies that files match the original data.
         /// </summary>
         Verify = NativeMethods.WIM_FLAG_VERIFY,
+
         /// <summary>
         /// The image is to be sequentially read for caching or performance purposes.
         /// </summary>
         Index = NativeMethods.WIM_FLAG_INDEX,
+
         /// <summary>
         /// Performs application, but does not actually creating files or directories.
         /// Messages are still raised, useful for getting a list of files and directories in the image.
         /// </summary>
         // TODO: investigate if messages are actually still raised
         NoApply = NativeMethods.WIM_FLAG_NO_APPLY,
+
         /// <summary>
         /// Send a WIM_MSG_FILEINFO message during the apply operation.
         /// </summary>
         // TODO: replace WIM_MSG_FILEINFO w/ a <see> to the message type
         SendFileInfoDuringApply = NativeMethods.WIM_FLAG_FILEINFO,
+
         /// <summary>
         /// Disable automatic fixups of reparse points (junctions and symlinks).
         /// </summary>
         DisableReparsePointFix = NativeMethods.WIM_FLAG_NO_RP_FIX,
+
         /// <summary>
         /// Do not restore ACLs for directories.
         /// </summary>
         DontRestoreDirectoryAcls = NativeMethods.WIM_FLAG_NO_DIRACL,
+
         /// <summary>
         /// Do not restore ACLs for files.
         /// </summary>
@@ -83,14 +94,17 @@ namespace ManagedWimgapi {
         /// No compression.
         /// </summary>
         None = 0,
+
         /// <summary>
         /// Xpress compression.
         /// </summary>
         Xpress = 1,
+
         /// <summary>
         /// Lzx compression.
         /// </summary>
         Lzx = 2,
+
         /// <summary>
         /// Lzms compression.
         /// </summary>
@@ -106,10 +120,12 @@ namespace ManagedWimgapi {
         /// Do not use any special options.
         /// </summary>
         None = 0,
+
         /// <summary>
         /// Generates data integrity information for new files. Verifies and updates existing files.
         /// </summary>
         Verify = NativeMethods.WIM_FLAG_VERIFY,
+
         /// <summary>
         /// Opens the .wim file in a mode that enables simultaneous reading and writing.
         /// </summary>
@@ -124,6 +140,7 @@ namespace ManagedWimgapi {
         /// The file did not exist and was created.
         /// </summary>
         CreatedNew = 0,
+
         /// <summary>
         /// The file existed and was opened for access.
         /// </summary>
@@ -138,14 +155,17 @@ namespace ManagedWimgapi {
         /// Makes a new image file. Fails if the specified file already exists.
         /// </summary>
         CreateNew = 1,
+
         /// <summary>
         /// Makes a new image file. Overwrites if the file already exists.
         /// </summary>
         CreateAlways = 2,
+
         /// <summary>
         /// Opens the image file. Fails if the file does not exist.
         /// </summary>
         OpenExisting = 3,
+
         /// <summary>
         /// Opens the image file if it exists. If the file does not exist and the caller requests <see cref="WimAccess.Write"/> access, the function makes the file.
         /// </summary>
